c21b021d39e45a1616f9e9294f7d78fbc710bab4
Yunus Emre FIRAT <yemre.firat@gmail.com>
2019-10-06 10:58:37 +0300

    Handle sending search request

--- app/src/main/kotlin/com/vngrs/githubchallange/search/SearchActivity.kt
+++ app/src/main/kotlin/com/vngrs/githubchallange/search/SearchActivity.kt
@@ -1,13 +1,19 @@
 package com.vngrs.githubchallange.search
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.vngrs.githubchallange.R
 import com.vngrs.githubchallange.model.SearchDataSource
 
-class SearchActivity : AppCompatActivity(), SearchContract.ViewInterface {
+class SearchActivity : AppCompatActivity(),
+    SearchContract.ViewInterface,
+    SearchView.OnQueryTextListener {
 
     private lateinit var searchPresenter: SearchContract.PresenterInterface
+
+    private var page = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +22,24 @@ class SearchActivity : AppCompatActivity(), SearchContract.ViewInterface {
         setupPresenter()
     }
 
-    override fun onStart() {
-        super.onStart()
-        //TODO: add real method parameters after search screen view implementation
-        searchPresenter.getSearchResults("retrofit", "1")
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu.findItem(R.id.menu_item_search)
+        val searchView = searchMenuItem.actionView as SearchView
+
+        with(searchView) {
+            queryHint = resources.getString(R.string.search_repositories_hint)
+            isSubmitButtonEnabled = true
+            setOnQueryTextListener(this@SearchActivity)
+
+            setIconifiedByDefault(false)
+            isFocusable = true
+            requestFocus()
+        }
+
+        return true
     }
 
     override fun onStop() {
@@ -30,5 +50,14 @@ class SearchActivity : AppCompatActivity(), SearchContract.ViewInterface {
     private fun setupPresenter() {
         val dataSource: SearchDataSource = SearchDataSource()
         searchPresenter = SearchPresenter(this, dataSource)
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextSubmit(query: String): Boolean {
+        searchPresenter.getSearchResults(query, page.toString())
+        return true
     }
 }
