d0c39ca8b71e7dbf4dcc64a8b83b061d1039dc22
Yunus Emre FIRAT <yemre.firat@gmail.com>
2019-10-07 20:26:32 +0300

    Handle displaying profile screen

--- app/src/main/kotlin/com/vngrs/githubchallange/profile/ProfileActivity.kt
+++ app/src/main/kotlin/com/vngrs/githubchallange/profile/ProfileActivity.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
+import androidx.recyclerview.widget.RecyclerView
 import com.vngrs.githubchallange.R
 import com.vngrs.githubchallange.model.ProfileDataSource
 import com.vngrs.githubchallange.model.Repository
@@ -14,6 +17,11 @@ class ProfileActivity : AppCompatActivity(),
 
     private lateinit var profilePresenter: ProfileContract.PresenterInterface
 
+    private lateinit var profileRecyclerView: RecyclerView
+    private lateinit var profileAdapter: ProfileAdapter
+
+    private lateinit var user: UserResponse
+    private var repositoriesList = mutableListOf<Repository>()
     private lateinit var username: String
     private var page = 1
 
@@ -24,6 +32,7 @@ class ProfileActivity : AppCompatActivity(),
         username = intent.getStringExtra(KEY_USERNAME)
 
         setupPresenter()
+        initUserInterface()
     }
 
     override fun onStart() {
@@ -35,15 +44,31 @@ class ProfileActivity : AppCompatActivity(),
         profilePresenter = ProfilePresenter(this, ProfileDataSource())
     }
 
+    private fun initUserInterface() {
+        profileRecyclerView = findViewById(R.id.activity_profile_recyclerview)
+        val dividerItemDecoration = DividerItemDecoration(this, VERTICAL)
+        dividerItemDecoration.setDrawable(resources.getDrawable(R.drawable.profile_item_divider, null))
+        profileRecyclerView.addItemDecoration(dividerItemDecoration)
+    }
+
     override fun displayProfileResult(userResponse: UserResponse) {
-        //TODO: will be handled later
+        this.user = userResponse
         if (page == 1) {
             profilePresenter.getRepositoriesResult(username, page.toString())
         }
     }
 
     override fun displayRepositoriesResult(repositoryList: List<Repository>) {
-        //TODO: will be handled later
+        page++
+        if (repositoriesList.isNullOrEmpty()) {
+            repositoriesList.addAll(repositoryList)
+            profileAdapter = ProfileAdapter(user, repositoriesList)
+            profileRecyclerView.adapter = profileAdapter
+            return
+        }
+
+        repositoriesList.addAll(repositoryList)
+        profileAdapter.notifyDataSetChanged()
     }
 
     companion object {
